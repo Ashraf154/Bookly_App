92a60aaf55f9ece356d6e4ec09c35321efc7b46a
Ashraf154 <your-email@example.com>
2026-09-20 17:51:48 +0300

    create app styles

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presentation/view/widgets/feature_book_list_view_item.dart';
@@ -8,16 +9,57 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      CustomAppBar(),
-     
-     FeatureBooksListView(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(children: [
+        CustomAppBar(),
+       
+       FeatureBooksListView(),
+      
+       SizedBox(height: 50,),
+      
+       Text('Best Seller',
+       style: Styles.textStyle18,
+       ),
+      
+       BestSellerListViewItem(),
+       
+       ]),
+    );
+  }
+}
 
-     SizedBox(height: 50,),
+class BestSellerListViewItem extends StatelessWidget {
+  const new({super.key});
 
-     Text('Best Seller',style: Styles.titleMedium,),
-     
-     ]);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+           AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          width: 50,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(AssetsData.testImage),
+            ),
+          ),
+        ),
+      ),
+      Column(
+        children: [
+
+        ],
+      ),
+        ],
+      ),
+    );
   }
 }
 
