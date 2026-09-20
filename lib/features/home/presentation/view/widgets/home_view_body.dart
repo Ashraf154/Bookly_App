ba64f12e2d02e1114097af1c0576aee906d681a3
Ashraf154 <your-email@example.com>
2026-09-20 16:53:16 +0300

    add google font to text theme

--- lib/features/home/presentation/view/widgets/home_view_body.dart
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -1,7 +1,6 @@
-
-import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/view/widgets/custom_app_bar.dart';
-import 'package:bookly/features/home/presentation/view/widgets/custom_list_view_item.dart';
+import 'package:bookly/features/home/presentation/view/widgets/feature_book_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,12 +8,17 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-     children: [
-    CustomAppBar(),
-    CustomListViewItem(),
-     ],
-    );
+    return Column(children: [
+      CustomAppBar(),
+     
+     FeatureBooksListView(),
+
+     SizedBox(height: 50,),
+
+     Text('Best Seller',style: Styles.titleMedium,),
+     
+     ]);
   }
 }
+
 
