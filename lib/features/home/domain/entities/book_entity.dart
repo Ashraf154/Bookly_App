ca09811f592cbc18e5d1a6f147eea44c19c33465
Ashraf154 <your-email@example.com>
2026-09-21 20:37:00 +0300

    create home repo && entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  new(this.image, this.title, this.authorName, this.price, this.rating);
+}
